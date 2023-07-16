281919d7e3c3accc993761065de178f003c34e1d
Lautaro Rodriguez <81494428+LautareteX@users.noreply.github.com>
2023-07-16 12:15:13 -0300

    Update index.js

--- 01 App Node/index.js
+++ 01 App Node/index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-const hostname = '172.17.0.2'; /*Container 01-aoo-node IP Address*/
+const hostname = '172.17.0.2'; /*Container 01-app-node IP Address*/
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log("Request from ", req.socket.remoteAddress);
+  console.log("Request from ", req.socket.remoteAddress); /*Getting IP Address from request*/
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
