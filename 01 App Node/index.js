6bc551810e6b6bd23b0045b07a06ad29e2a1d497
Lautaro Rodriguez <81494428+LautareteX@users.noreply.github.com>
2023-07-16 12:13:01 -0300

    Add files via upload

---
+++ 01 App Node/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '172.17.0.2'; /*Container 01-aoo-node IP Address*/
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("Request from ", req.socket.remoteAddress);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
